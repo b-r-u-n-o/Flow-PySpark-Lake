db2fd2b8d4403e356522dd3f00d851c69960f239
Bruno  Soares <tsoares.bruno@gmail.com>
2021-11-10 16:28:48 -0300

    commint inicial

---
+++ etl/ingest.py
@@ -0,0 +1,121 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.dataframe import DataFrame
+from pyspark.sql.functions import current_date
+import logging
+import os
+
+# TODO:
+# Extrair os arquivos CSV da camada Stage para Raw
+# Adicionar os campos DT_CARGA (tipo DATE)
+# Enconding UTF-8
+
+
+def load_csv_file(spark: SparkSession, path: str) -> DataFrame:
+    """Efetua a carga de dados no formato csv
+
+    Args:
+        spark: objeto SparkSession
+        path (str): caminho de origem do arquivo a ser carregado
+
+    Returns:
+        DataFrameReader: retorna um Spark Dataframe
+    """
+
+    output_df = spark.read.options(
+        delimiter="|", header=True, encoding="UTF-8", inferSchema=True
+    ).csv(path)
+
+    return output_df
+
+
+def insert_current_date(df: DataFrame) -> DataFrame:
+    """Insere campo com a idade calculada no df
+
+    Args:
+        df (DataFrame): Dataframe com campo que possa ser utilizado na criação do campo calculado de idade
+
+    Returns:
+        DataFrame: com  a inserção de um campo calculado de idade
+    """
+    output_df = df.withColumn("DT_CARGA", current_date())
+
+    return output_df
+
+
+def save_parquet_file(
+    df: DataFrame,
+    division_data: int,
+    parquet_path: str,
+    mode: str,
+    compression: str,
+    partition_by=None,
+) -> None:
+    """Salva o df no formato parquet
+
+    Args:
+        df (DataFrame): Dataframe a ser salvo no formato parquet
+        division_data (int): indica o parametro utilizado pelo método coalesce
+        parquet_path (str): local onde o arquivo deverá ser salvo
+        mode (str): 'append', 'overwrite'
+        partition_by (None): nome do campo a ser usado na partição, ou, uma lista de nomes de campos
+        compression (str): informar um tipo de compressão para o salvamento
+    """
+    df.coalesce(division_data).write.parquet(
+        path=parquet_path, mode=mode, partitionBy=partition_by, compression=compression
+    )
+
+
+if __name__ == "__main__":
+
+    ROOT_PATH = "../lake/"
+    STAGE_DB_EXAMES = f"{ROOT_PATH}stage/einstein_exames/EINSTEIN_Exames_2_082021.csv"
+    STAGE_DB_PACIENTES = (
+        f"{ROOT_PATH}stage/einstein_pacientes/EINSTEIN_Pacientes_2_082021.csv"
+    )
+    RAW_EXAMES_PATH = f"{ROOT_PATH}raw/einstein_exames/"
+    RAW_PACIENTES_PATH = f"{ROOT_PATH}raw/einstein_pacientes/"
+
+    logging.basicConfig(level=logging.INFO)
+    logger = logging.getLogger(__file__)
+    logger.setLevel(logging.INFO)
+
+    spark = SparkSession.builder.appName("app_einstein").getOrCreate()
+    logger.info("Spark session iniciada!")
+
+    tbl_exames = load_csv_file(spark=spark, path=STAGE_DB_EXAMES)
+    logger.info(f"Carga do arquivo de exames da camada STAGE realizada")
+
+    insere_data_exames = insert_current_date(df=tbl_exames)
+    logger.info(f"Inserido o campo com a data da carga no dataset exames")
+
+    os.makedirs(name=RAW_EXAMES_PATH, exist_ok=True)
+    save_parquet_file(
+        df=insere_data_exames,
+        division_data=1,
+        mode="overwrite",
+        partition_by="DT_COLETA",
+        parquet_path=RAW_EXAMES_PATH,
+        compression="snappy",
+    )
+    logger.info(f"Arquivo em formato parquet, salvo na camada RAW")
+
+    tbl_pacientes = load_csv_file(spark=spark, path=STAGE_DB_PACIENTES)
+    logger.info(f"Carga do arquivo de pacientes da camada STAGE realizada")
+
+    insere_data_pacientes = insert_current_date(df=tbl_pacientes)
+    logger.info(f"Inserido o campo com a data da carga")
+
+    os.makedirs(name=RAW_PACIENTES_PATH, exist_ok=True)
+    save_parquet_file(
+        df=insere_data_pacientes,
+        division_data=1,
+        mode="overwrite",
+        partition_by="CD_UF",
+        parquet_path=RAW_PACIENTES_PATH,
+        compression="snappy",
+    )
+    logger.info(f"Arquivo salvo em formato parquet na camada RAW do Data Lake")
+
+    logger.info(f"Cargas realizadas com sucesso!")
+    spark.stop()
+    logger.info("Encerrada sessão Spark.")
---
+++ etl/processed.py
@@ -0,0 +1,129 @@
+import os
+from pyspark.sql import SparkSession
+from pyspark.sql.dataframe import DataFrame
+from pyspark.sql.functions import *
+from ingest import save_parquet_file
+import logging
+
+
+# TODO: Criar um novo dataset
+# Realizar um join com as bases exames e pacientes
+# Usar o ID_PACIENTE como chave
+# PACIENTES -> Desconsiderar os campos AA_NASCIMENTO e DT_CARGA
+#              Criar um campo calculado para VL_IDADE
+# EXAMES -> Desconsiderar DT_CARGA
+# Criar exames_por_pacientes na camada Service
+# Criar exames_por_pacientes_sp na camada Service
+
+
+def load_parquet_file(spark: SparkSession, parquet_path: str) -> DataFrame:
+    """Efetua a carga de arquivo do tipo parquet
+
+    Args:
+        spark (SparkSession): SparkSession
+        parquet_path (str): caminho de origem do arquivo to/path/parquetfile
+
+    Returns:
+        DataFrame
+    """
+    output_df = spark.read.parquet(parquet_path)
+    return output_df
+
+
+def join_data(
+    df: DataFrame, other_df: DataFrame, on_column: str, method: str
+) -> DataFrame:
+    """[summary]
+
+    Args:
+        df (DataFrame): dataframe principal
+        other_df (DataFrame): dataframe a ser juntado
+        on_column (str): coluna chave para a junção
+        method (str): método para a realização ('left', 'inner', 'outer', 'right')
+
+    Returns:
+        DataFrame: resultado da junção
+    """
+    output_df = df.join(other_df, on_column, method)
+    return output_df
+
+
+def insert_calculated_age(df: DataFrame, new_column: str, ref_column: str) -> DataFrame:
+    """[summary]
+
+    Args:
+        df (DataFrame): dataframe alvo
+        new_column (str): nome da nova coluna
+        ref_column (str): coluna de referência
+
+    Returns:
+        DataFrame: modificado com a nova coluna
+    """
+    output_df = df.withColumn(
+        new_column, round(months_between(current_date(), col(ref_column)) / lit(12))
+    )
+    return output_df
+
+
+if __name__ == "__main__":
+
+    logging.basicConfig(level=logging.INFO)
+    logger = logging.getLogger(__file__)
+    logger.setLevel(logging.INFO)
+
+    PATH_RAW_EXAMES = "../lake/raw/einstein_exames"
+    PATH_RAW_PACIENTES = "../lake/raw/einstein_pacientes"
+    PATH_SERVICE_EXM_PACIENTES = "../lake/service/exames_por_pacientes"
+
+    PATH_SERVICE_EXM_PACIENTES_SP = "../lake/service/exames_por_pacientes_sp"
+
+    spark = SparkSession.builder.appName("appProcessed").getOrCreate()
+    logger.info("Spark session criada")
+
+    df_exames = load_parquet_file(spark=spark, parquet_path=PATH_RAW_EXAMES)
+    df_pacientes = load_parquet_file(spark=spark, parquet_path=PATH_RAW_PACIENTES)
+    logger.info("Dados carregados com sucesso")
+
+    join_df_pacientes_exames = join_data(
+        df=df_pacientes, other_df=df_exames, on_column=["ID_PACIENTE"], method="left"
+    )
+    logger.info("Join entre os dados de exames e pacientes realizado")
+
+    insere_idade_paciente_df = insert_calculated_age(
+        df=join_df_pacientes_exames, new_column="VL_IDADE", ref_column="AA_NASCIMENTO"
+    )
+    logger.info("Campo calculado com a idade dos pacientes criado")
+
+    df_exames_pacientes_ajustado = insere_idade_paciente_df.drop(
+        "DT_CARGA", "AA_NASCIMENTO"
+    )
+    logger.info("Subset exames por pacientes criado")
+
+    os.makedirs(name=PATH_SERVICE_EXM_PACIENTES, exist_ok=True)
+    salvadf_exames_pacientes = save_parquet_file(
+        df=df_exames_pacientes_ajustado,
+        parquet_path=PATH_SERVICE_EXM_PACIENTES,
+        mode="overwrite",
+        partition_by=["CD_PAIS", "CD_UF"],
+        compression="snappy",
+        division_data=1,
+    )
+    logger.info("Novo dataset criado: exames_por_pacientes")
+
+    novo_df_exames_pacientes_sp = load_parquet_file(
+        spark=spark, parquet_path=f"{PATH_SERVICE_EXM_PACIENTES}/CD_PAIS=BR/CD_UF=SP"
+    )
+    logger.info("Carga do dataset exames_por_pacientes")
+
+    os.makedirs(name=PATH_SERVICE_EXM_PACIENTES_SP, exist_ok=True)
+    save_parquet_file(
+        df=novo_df_exames_pacientes_sp,
+        parquet_path=PATH_SERVICE_EXM_PACIENTES_SP,
+        mode="overwrite",
+        compression="snappy",
+        division_data=1,
+    )
+    logger.info("Criado novo dataset: exames_por_pacientes_sp")
+
+    spark.stop()
+    logger.info("Finalizada sessão Spark.")
